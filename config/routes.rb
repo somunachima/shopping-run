0222787f038e18d7fb159f171741d2a0b33906f9
Som Akudu <somakudu@gmail.com>
2023-05-28 14:31:34 +0100

    create items index page

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
-  get 'items/new'
-  get 'users/new'
+  # get 'items/new'
+  # get 'users/new'
+  root to: "items#index"
   devise_for :users
-  root to: "pages#home"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :items
   # Defines the root path route ("/")
   # root "articles#index"
 end
