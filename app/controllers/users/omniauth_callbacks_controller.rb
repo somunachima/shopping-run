a70a47443497750f49a815bf936874526d51dfb9
Som Akudu <somakudu@gmail.com>
2023-05-28 20:55:23 +0100

    add from_omniauth method to user model

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -31,4 +31,10 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   # def after_omniauth_failure_path_for(scope)
   #   super(scope)
   # end
+
+  private
+
+  def auth
+    @auth ||= request.env['omniauth.auth']
+  end
 end
