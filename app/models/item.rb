abffa14322b2209de8d69ed9d4519b9328cacae7
Som Akudu <somakudu@gmail.com>
2023-05-29 01:58:53 +0100

    add conditional statement so that delete all button only appears if items are on the list

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,6 +2,6 @@ class Item < ApplicationRecord
   belongs_to :user
 
   validates :name, presence: { message: 'Must enter item name' }
-  validates :name, uniqueness: { message: 'Item already exists' }
+  # validates :name, uniqueness: { case_sensitive: false, message: 'Item already exists' }
 
 end
