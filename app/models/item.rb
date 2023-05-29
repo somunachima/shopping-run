d09d3cc9d45ecc2aec4eb2be95f262b3069224d0
Som Akudu <somakudu@gmail.com>
2023-05-29 01:40:57 +0100

    add validations for item to prevent user from adding duplicate items and blank items

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,7 @@
 class Item < ApplicationRecord
   belongs_to :user
 
-  validates :name, presence: true
+  validates :name, presence: { message: 'Must enter item name' }
+  validates :name, uniqueness: { message: 'Item already exists' }
+
 end
