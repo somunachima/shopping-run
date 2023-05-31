a47805e1e25c73ef3b7f4ff3ca5f76e6b91a7b34
Som Akudu <somakudu@gmail.com>
2023-05-31 12:07:01 +0100

    fix bookmark typo in bookmarks controller for create method

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -2,25 +2,12 @@ class BookmarksController < ApplicationController
   before_action :set_bookmark, only: [:new, :create, :destroy]
   def create
     @bookmark = current_user.bookmarks.new(bookmark_params)
-    # @bookmark = Bookmark.new(bookmark_params)
-    # @bookmark.item = @item
     if !@bookmark.save
       flash[:notice] = @bookmark
     end
 
     redirect_to items_path(current_user)
   end
-
-  # def create
-  #   @teacher = Teacher.find(params[:teacher_id])
-  #   @review = Review.new(review_params)
-  #   @review.teacher = @teacher
-  #   if @review.save
-  #     redirect_to teacher_path(@teacher)
-  #   else
-  #     render :new, status: :unprocessable_entity
-  #   end
-  # end
 
   def destroy
     @bookmark = current_user.bookmark.find(params[:id])
